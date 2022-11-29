4bc17252c4e814b0eef831d0e2bf7d0f4ad8e09d
aisyaaaicha <20081010083@student.upnjatim.ac.id>
2022-11-29 13:47:00 +0700

    update lagi

--- lib/main.dart
+++ lib/main.dart
@@ -22,13 +22,19 @@ import 'package:ditonton/ssl_pinning.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:ditonton/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:ditonton/injection.dart' as di;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await SslPinning.init();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   di.init();
+
   runApp(MyApp());
 }
 
